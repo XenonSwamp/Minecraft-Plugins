dcd42ce53b3e56b48d114ce7b5c33c342c5d5574
Heathrxw <Heathrxw@gmail.com>
2021-08-22 16:29:27 +0100

    1 hp event

---
+++ ChaosMod Lite/src/main/java/Heathrxw/Xenon/ChaosMod/events/OneHPEvent.java
@@ -0,0 +1,20 @@
+package Heathrxw.Xenon.ChaosMod.events;
+
+import Heathrxw.Xenon.ChaosMod.Chaos;
+import Heathrxw.Xenon.ChaosMod.ChaosEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class OneHPEvent extends ChaosEvent {
+    public void onEnable(String username, Chaos plugin) {
+        Player target = (Bukkit.getServer().getPlayer(username));
+
+        if(target != null) {
+            target.setHealth(1);
+        }
+    }
+
+    public String getName() {
+        return "1 HP";
+    }
+}
