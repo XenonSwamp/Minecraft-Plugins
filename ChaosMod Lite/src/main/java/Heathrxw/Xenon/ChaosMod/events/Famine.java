530a66e32be97fdf5eb8084389426637d961e143
Heathrxw <Heathrxw@gmail.com>
2021-08-22 16:27:37 +0100

    Famine event

---
+++ ChaosMod Lite/src/main/java/Heathrxw/Xenon/ChaosMod/events/Famine.java
@@ -0,0 +1,21 @@
+package Heathrxw.Xenon.ChaosMod.events;
+
+import Heathrxw.Xenon.ChaosMod.Chaos;
+import Heathrxw.Xenon.ChaosMod.ChaosEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+
+public class Famine extends ChaosEvent {
+    public void onEnable(String username, Chaos plugin) {
+        Player target = (Bukkit.getServer().getPlayer(username));
+
+        if(target != null) {
+            target.setFoodLevel(1);
+            target.setSaturation(1f);
+        }
+    }
+
+    public String getName() {
+        return "Famine";
+    }
+}
