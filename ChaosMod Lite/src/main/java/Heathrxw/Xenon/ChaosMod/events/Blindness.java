3c0570e6510e5971d23106438d63755b55a3b372
Heathrxw <Heathrxw@gmail.com>
2021-08-22 16:27:47 +0100

    Blindness event

---
+++ ChaosMod Lite/src/main/java/Heathrxw/Xenon/ChaosMod/events/Blindness.java
@@ -0,0 +1,30 @@
+package Heathrxw.Xenon.ChaosMod.events;
+
+import Heathrxw.Xenon.ChaosMod.Chaos;
+import Heathrxw.Xenon.ChaosMod.ChaosEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
+
+public class Blindness extends ChaosEvent {
+    public void onEnable(String username, Chaos plugin) {
+        Player target = (Bukkit.getServer().getPlayer(username));
+
+        if(target != null) {
+            target.addPotionEffect(new PotionEffect(PotionEffectType.BLINDNESS, Integer.MAX_VALUE, 1));
+        }
+    }
+
+    public void onDisable(String username, Chaos plugin) {
+        Player target = (Bukkit.getServer().getPlayer(username));
+
+        if(target != null) {
+            target.removePotionEffect(PotionEffectType.BLINDNESS);
+        }
+    }
+
+    public String getName() {
+        return "Blind";
+    }
+}
