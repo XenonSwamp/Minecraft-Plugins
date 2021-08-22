55845eaadd5378475960692683235a236785c6fe
Heathrxw <Heathrxw@gmail.com>
2021-08-22 16:29:40 +0100

    undead  event

---
+++ ChaosMod Lite/src/main/java/Heathrxw/Xenon/ChaosMod/events/Undead.java
@@ -0,0 +1,25 @@
+package Heathrxw.Xenon.ChaosMod.events;
+
+import Heathrxw.Xenon.ChaosMod.Chaos;
+import Heathrxw.Xenon.ChaosMod.ChaosEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+public class Undead extends ChaosEvent {
+    public void onEnable(String username, Chaos plugin) {
+        Player target = (Bukkit.getServer().getPlayer(username));
+
+        if(target != null) {
+            target.getWorld().spawnEntity(target.getLocation().add(0, 3, 0), EntityType.ZOMBIE);
+            target.getWorld().spawnEntity(target.getLocation().add(0, 3, 0), EntityType.ZOMBIE);
+            target.getWorld().spawnEntity(target.getLocation().add(0, 3, 0), EntityType.ZOMBIE);
+            target.getWorld().spawnEntity(target.getLocation().add(0, 3, 0), EntityType.ZOMBIE);
+            target.getWorld().spawnEntity(target.getLocation().add(0, 3, 0), EntityType.ZOMBIE);
+        }
+    }
+
+    public String getName() {
+        return "Zombies";
+    }
+}
